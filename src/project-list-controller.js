cb10b8b366c24c627ccee0e8fc0ace82c7d6ab26
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-09 17:49:31 +0700

    add initProjectListZZ

--- src/project-list-controller.js
+++ src/project-list-controller.js
@@ -1,10 +1,21 @@
 import { projectBuilder } from "./project-builder.js";
 import { pubsub } from "./pubsub.js";
 
-export const projectListController=(function(){
-   let projectList = [];
-   
-   function getProjectList(){
-       return projectList;
-   }
+export const projectListController = (function () {
+  let projectList = [];
+
+  function getProjectList() {
+    return projectList;
+  }
+
+  function initProjectList(storedList) {
+    projectList = storedList || buildDefaultList();
+  }
+
+  function buildDefaultList() {
+    let list = [];
+    const defaultProject = projectBuilder("My Project", [], true);
+    list.push(defaultProject);
+    return list;
+  }
 })();
