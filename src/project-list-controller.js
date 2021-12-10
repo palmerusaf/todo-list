f67b777a7fd7804a93aeb7bf275eefae75458a7d
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-10 15:42:06 +0700

    add deleteProject function

--- src/project-list-controller.js
+++ src/project-list-controller.js
@@ -4,6 +4,7 @@ import { pubsub } from "./pubsub.js";
 export const projectListController = (function () {
   let projectList = [];
   pubsub.subscribe("loadProjectList", initProjectList);
+  pubsub.subscribe("projectDeleteClick", deleteProject);
 
   function getProjectList() {
     return projectList;
@@ -19,5 +20,13 @@ export const projectListController = (function () {
     const defaultProject = projectBuilder("My Project", [], true);
     list.push(defaultProject);
     return list;
+  }
+
+  function deleteProject(clickEvent) {
+    const index = clickEvent; //change to clickEvent.index after render module implemented
+    projectList[index]
+      ? projectList.splice(index, 1)
+      : console.warn(`Project index ${index} not found to delete.`);
+    pubsub.publish("updateListOfProjects", getProjectList());
   }
 })();
--- src/tests/project-list-controller-tests.js
+++ src/tests/project-list-controller-tests.js
@@ -13,3 +13,8 @@ function logData(data){
 // pubsub.publish("loadProjectList",JSON.parse(localStorage.getItem("projectList"))||"");
 // Test with local storage
 // pubsub.publish("loadProjectList",projectListPrototype);
+
+// removeProject Tests
+// pubsub.publish("loadProjectList",projectListPrototype);
+// pubsub.publish("projectDeleteClick",2)
+// pubsub.publish("projectDeleteClick",2)
