a46fa2559d6ad6198581c053c233dab8922cc7ef
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-15 17:33:15 +0700

    add makeAddItemButton func

--- src/render.js
+++ src/render.js
@@ -1,8 +1,22 @@
 export const Render = (() => {
-  const makeAddItemButton = () => {};
-  const makeEditItemButton = () => {};
-  const makeDeleteItemButton = () => {};
-  const makeSubmitItemButton = () => {};
+  const makeAddItemButton = () => {
+    const button = document.createElement("button");
+    button.textContent = "+";
+    button.classList.add("add-item-button");
+    return button;
+  };
+  const makeEditItemButton = () => {
+    const button = document.createElement("button");
+    return button;
+  };
+  const makeDeleteItemButton = () => {
+    const button = document.createElement("button");
+    return button;
+  };
+  const makeSubmitItemButton = () => {
+    const button = document.createElement("button");
+    return button;
+  };
 
   return {
     makeAddItemButton,
