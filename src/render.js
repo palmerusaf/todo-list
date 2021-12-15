5095fe373c5b5f9253037ef882082bc58184112a
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-15 18:40:26 +0700

    add function to link google icons stylesheet

--- src/render.js
+++ src/render.js
@@ -1,18 +1,32 @@
 export const Render = (() => {
+  // Link stylesheet for delete icon
+  function linkToGoogleIconsStylesheet() {
+    const link = document.createElement("link");
+    link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+    link.rel = "stylesheet";
+    return link;
+  }
+  document.head.appendChild(linkToGoogleIconsStylesheet());
+
   const makeAddItemButton = () => {
     const button = document.createElement("button");
     button.textContent = "+";
     button.classList.add("add-item-button");
     return button;
   };
+
   const makeEditItemButton = () => {
     const button = document.createElement("button");
+    button.classList.add("material-icons");
+    button.textContent = "edit";
     return button;
   };
+
   const makeDeleteItemButton = () => {
     const button = document.createElement("button");
     return button;
   };
+
   const makeSubmitItemButton = () => {
     const button = document.createElement("button");
     return button;
--- src/tests/render-tests.js
+++ src/tests/render-tests.js
@@ -1,1 +1,2 @@
-import "../render.js";
+import { Render } from "../render.js";
+document.body.appendChild(Render.makeEditItemButton());
