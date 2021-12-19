3288384fcf4b2573dda7eaab183be5acbecdf5e1
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-19 15:06:33 +0700

    add makeTaskItem

--- src/render-task-field.js
+++ src/render-task-field.js
@@ -34,7 +34,27 @@ export const RenderTaskField = (() => {
     function _makeTaskList(project) {
       const list = document.createElement("div");
       list.classList.add(tf("task-list"));
+      project.taskList.forEach((task) => list.appendChild(_makeTaskItem(task)));
       return list;
+
+      function _makeTaskItem(task) {
+        const item = document.createElement("span");
+        item.classList = tf("task-item", {
+          ["mark-done"]: task.taskCompleteStatus,
+        });
+        item.appendChild(_makeTaskMainView(task));
+        item.appendChild(_makeTaskExpandedView(task));
+        return item;
+      }
+
+      function _makeTaskMainView(task) {
+        const container = document.createElement("div");
+        return container;
+      }
+      function _makeTaskExpandedView(task) {
+        const container = document.createElement("div");
+        return container;
+      }
     }
 
     function _getActiveProject(projectList) {
@@ -44,3 +64,10 @@ export const RenderTaskField = (() => {
 
   return { taskField };
 })();
+// {
+// "title": "task0 for project b",
+// "description": "description0",
+// "dueDate": "1989-10-24T17:00:00.000Z",
+// "priority": "0",
+// "taskCompleteStatus": false
+//   }
