0863e292aa303dd5c8df465d2073396488fd4e19
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-19 13:47:52 +0700

    add renderTaskFieldContent

--- src/render-task-field.js
+++ src/render-task-field.js
@@ -8,12 +8,35 @@ export const RenderTaskField = (() => {
   const tf = bem("task-field");
 
   // Initialize PubSubs
-  //   pubsub.subscribe("updateListOfProjects", renderTaskFieldContent);
+  pubsub.subscribe("updateListOfProjects", renderTaskFieldContent);
 
   // Initialize taskField
   const taskField = document.createElement("div");
   taskField.classList.add(tf());
   taskField.textContent = "test";
+
+  function renderTaskFieldContent(projectList) {
+    const project = _getActiveProject(projectList);
+    taskField.textContent = "";
+    if (!project) return;
+    taskField.appendChild(_makeProjectTitle(project));
+    taskField.appendChild(_makeTaskList(project));
+    return taskField;
+
+    function _makeProjectTitle(project) {
+      const title = document.createElement("h1");
+      title.classList.add(tf("title"));
+      return title;
+    }
+    function _makeTaskList(project) {
+      const list = document.createElement("div");
+      list.classList.add(tf("task-list"));
+      return list;
+    }
+    function _getActiveProject(projectList) {
+      return projectList.filter((project) => project.activeStatus);
+    }
+  }
 
   return { taskField };
 })();
