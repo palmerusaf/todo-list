70d0187ba6def0577683b12e53a5f4df3b570e39
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-27 13:08:48 +0700

    add pubsubs, get and save functions

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,3 +1,14 @@
 import { pubsub } from "./pubsub.js";
 
-export const LocalStorage = (() => {})();
+export const LocalStorage = (() => {
+  pubsub.publish("loadProjectList", _getSavedData());
+  pubsub.subscribe("updateListOfProjects", _saveDataLocally);
+
+  function _getSavedData() {
+    return JSON.parse(localStorage.getItem("savedData"));
+  }
+
+  function _saveDataLocally(data) {
+    localStorage.setItem("savedData", JSON.stringify(data));
+  }
+})();
