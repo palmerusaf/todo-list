3f91cbf5e5ead28bd2fbee274c76cb243db9d11b
Branden Palmer <brandenpalmer08@gmail.com>
2021-12-28 19:55:29 +0700

    change config to prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       title: "TO-DO List",
