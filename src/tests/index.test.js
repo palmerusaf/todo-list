897a4c9972fa00235207f6f0fb9270b2dd10329c
Branden Palmer <brandenpalmer08@gmail.com>
2022-07-16 12:05:53 +0700

    get snapshots to work

---
+++ src/tests/index.test.js
@@ -0,0 +1,19 @@
+import "../index";
+
+it("todo list and footer renders correctly", () => {
+  expect(document.body).toMatchSnapshot();
+});
+
+it.todo("New project is created correctly");
+
+it.todo("New project is updated correctly");
+
+it.todo("New project is deleted correctly");
+
+it.todo("Project is selected correctly");
+
+it.todo("New task is created correctly");
+
+it.todo("New task is updated correctly");
+
+it.todo("New task is deleted correctly");
